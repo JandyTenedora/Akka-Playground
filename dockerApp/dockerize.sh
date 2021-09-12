07444ae61c94374d2edc11fb2f7e576ec4d0b60e
jandytenedora <jandy.tenedora@hotmail.com>
2021-09-12 23:27:03 +1000

    Refactoring logging module into single file Fixing docker runs

--- dockerApp/dockerize.sh
+++ dockerApp/dockerize.sh
@@ -7,10 +7,10 @@ creating assembly================"
 cd ../ && sbt assembly
 echo "====================Coming back to dockerApp
 directory===================================="
-cd - echo "====================Copying application jar to
+cd dockerApp
+echo "====================Copying application jar to
 docker folder=============================="
-sudo cp ../target/scala-2.11/Hello-Akka-assembly-1.0.jar
-docker/
+sudo cp ../target/scala-2.11/akka-hello-assembly-1.0.jar docker/
 echo "====================Building docker
 image================================================="
 sudo docker build -t akkaapp .
--- src/main/scala/com/jandy/akkapractice/package5/devops/logging/Logging.scala
+++ src/main/scala/com/jandy/akkapractice/package5/devops/logging/Logging.scala
@@ -2,6 +2,18 @@ package com.jandy.akkapractice.package5.devops.logging
 
 import akka.actor.{ActorSystem, Props}
 
+import akka.actor.{Actor, ActorLogging}
+
+class LoggingActor extends Actor with ActorLogging{
+  def receive = {
+    case (a: Int, b: Int) => {
+      log.info(s"sum of $a and $b is ${a+b}")
+    }
+    case msg => log.warning(s"Not sure what you're talking about : $msg")
+  }
+
+}
+
 object Logging extends App{
   val actorSystem = ActorSystem("Akka")
   val loggingActor = actorSystem.actorOf(Props[LoggingActor], "loggingActor")
